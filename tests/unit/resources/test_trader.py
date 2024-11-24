c646444bdc48317f1f3e79c2946dc157dc826cd0
nickatnight <kay.muneee@gmail.com>
2024-11-23 18:54:09 -0800

    feat: add trader api [#1]

---
+++ tests/unit/resources/test_trader.py
@@ -0,0 +1,48 @@
+from unittest.mock import MagicMock
+
+from birdeyepy.resources.trader import Trader
+from birdeyepy.utils import BirdEyeApiUrls
+
+
+def test_trader_gainers_losers_api_expected_response() -> None:
+    # Arrange
+    mock_http = MagicMock()
+
+    # Act
+    client = Trader(http=mock_http)
+    client.gainers_losers()
+
+    # Assert
+    mock_http.send.assert_called_once_with(
+        path=BirdEyeApiUrls.TRADER_GAINERS_LOSERS,
+        params={
+            "time_frame": "1W",
+            "sort_by": "PnL",
+            "sort_type": "desc",
+            "offset": 0,
+            "limit": 10,
+        },
+    )
+
+
+def test_trader_seek_by_time_api_expected_response() -> None:
+    # Arrange
+    mock_http = MagicMock()
+
+    # Act
+    client = Trader(http=mock_http)
+    client.seek_by_time(address="test")
+
+    # Assert
+    mock_http.send.assert_called_once_with(
+        path=BirdEyeApiUrls.TRADER_SEEK_BY_TIME,
+        params={
+            "address": "test",
+            "tx_type": "swap",
+            "sort_type": "desc",
+            "offset": 0,
+            "limit": 100,
+            "before_time": 0,
+            "after_time": 0,
+        },
+    )
