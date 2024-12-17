5806cbf156d23a8d71062ad6bf010e0866526f37
nickatnight <kay.muneee@gmail.com>
2024-12-16 21:28:08 -0800

    test: add decorator test [#13]

--- tests/unit/resources/test_search.py
+++ tests/unit/resources/test_search.py
@@ -1,5 +1,7 @@
+# mypy: disable-error-code="attr-defined"
 from unittest.mock import MagicMock
 
+from birdeyepy.birdeye import BirdEye
 from birdeyepy.resources.search import Search
 from birdeyepy.utils import BirdEyeApiUrls
 
@@ -30,3 +32,20 @@ def test_search_token_market_data_called_with_expected_args() -> None:
             "markets": "test",
         },
     )
+
+
+def test_search_token_market_data_removes_header(mocker: MagicMock) -> None:
+    # Arrange
+    client = BirdEye(api_key="test")
+    mocker.patch.object(client.search.http, "send")
+
+    # Act
+    client.search.token_market_data(
+        keyword="test",
+        verify_token=True,
+        markets=["test"],
+    )
+
+    # Assert
+    assert "x-chain" in client.search.http.s.headers
+    assert client.search.http.s.headers["x-chain"] == "solana"
